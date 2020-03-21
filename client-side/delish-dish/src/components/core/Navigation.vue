c09cc7ede3409fa6fb8894fea88739fce9dda2f9
Katrin Yordanova <katrinyordanova@abv.bg>
2020-03-21 17:08:39 +0200

    added navigation

---
+++ client-side/delish-dish/src/components/core/Navigation.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="navbar navbar-expand-sm d-flex justify-content-center">
+        <img src="../../assets/chefff.png" class="cheff" alt="Responsive image">
+        <img src="../../assets/brand-logo.png" class="float-left" alt="Responsive image">
+        <b-navbar-brand href="#" class="brand">Delish Dish</b-navbar-brand>
+        <b-nav pills class="nav-links">
+            <b-nav-item active>Home</b-nav-item>
+            <b-nav-item active>Logout</b-nav-item>
+        </b-nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app-navigation'
+}
+</script>
+
+<style scoped>
+div, .brand {
+    color: rgb(255, 255, 255);
+}
+
+div {
+    background: rgb(255, 123, 0);
+    box-shadow: 0px 0px 5px 0px #5F5F5F;
+}
+
+img {
+    width: 4.5em;
+}
+
+.cheff {
+    width: 3.5em;
+    position: absolute;
+    left:2em;
+}
+
+.brand {
+    font-style: oblique;
+    font-family: 'Homemade Apple', serif;
+    font-size: 1.7em;
+}
+
+.brand:hover {
+    margin-bottom: 0.02em;
+    text-decoration: underline;
+    font-size: 1.7em;
+}
+
+.nav-links {
+    position: absolute; 
+    right: 1em;
+    margin-right: 0.5em;
+}
+
+.home-btn:hover {
+    color: green
+}
+
+#app > div > ul > li > a {
+    background: rgb(255, 255, 255);
+    color: rgb(255, 123, 0);
+}
+
+#app > div > ul > li:nth-child(1) > a {
+    margin-right: 0.7em;
+}
+</style>
--- client-side/delish-dish/src/main.js
+++ client-side/delish-dish/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { VBHoverPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VBHoverPlugin)
 
 new Vue({
   render: h => h(App),
