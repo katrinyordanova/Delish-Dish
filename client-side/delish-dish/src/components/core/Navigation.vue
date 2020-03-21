22a630400da83c848b69f458a85a65fe2012dac3
Katrin Yordanova <katrinyordanova@abv.bg>
2020-03-21 17:37:30 +0200

    tidied navigation

--- client-side/delish-dish/src/components/core/Navigation.vue
+++ client-side/delish-dish/src/components/core/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="navbar navbar-expand-sm d-flex justify-content-center">
-        <img src="../../assets/chefff.png" class="cheff" alt="Responsive image">
+        <img src="../../assets/chef.png" class="cheff" alt="Responsive image">
         <img src="../../assets/brand-logo.png" class="float-left" alt="Responsive image">
         <b-navbar-brand href="#" class="brand">Delish Dish</b-navbar-brand>
         <b-nav pills class="nav-links">
@@ -21,41 +21,43 @@ div, .brand {
     color: rgb(255, 255, 255);
 }
 
+.cheff, .nav-links {
+    position: absolute;
+}
+
 div {
     background: rgb(255, 123, 0);
     box-shadow: 0px 0px 5px 0px #5F5F5F;
+    padding: 0.3em
 }
 
 img {
     width: 4.5em;
+    margin-top: -0.3em
 }
 
 .cheff {
-    width: 3.5em;
-    position: absolute;
-    left:2em;
+    width: 3em;
+    left: 2em;
+    padding-top: 0.25em
 }
 
-.brand {
-    font-style: oblique;
+.brand, .brand:hover {
+font-style: oblique;
     font-family: 'Homemade Apple', serif;
-    font-size: 1.7em;
+    font-size: 2em;
+    padding-top: 0.55em;
+    margin-bottom: 0.02em;
 }
 
 .brand:hover {
-    margin-bottom: 0.02em;
     text-decoration: underline;
-    font-size: 1.7em;
 }
 
 .nav-links {
-    position: absolute; 
     right: 1em;
     margin-right: 0.5em;
-}
-
-.home-btn:hover {
-    color: green
+    font-size: 17.9px;
 }
 
 #app > div > ul > li > a {
